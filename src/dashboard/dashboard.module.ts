2dae57a6c76e345bca892f90e5be874dc46fd305
Yamoussa Keita <yamoussa.keita@lunion-lab.com>
2026-03-24 09:31:37 +0000

    modified:   src/dashboard/dashboard.module.ts

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DashboardService } from './dashboard.service';
+import { JobOffer } from 'src/job-offers/job-offer.entity';
 import { DashboardController } from './dashboard.controller';
+import { Candidature } from 'src/candidatures/candidature.entity';
+import { InterviewSession } from 'src/interview-sessions/interview-session.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([JobOffer, Candidature, InterviewSession])
+    ],
     controllers: [DashboardController],
     providers: [DashboardService],
 })
